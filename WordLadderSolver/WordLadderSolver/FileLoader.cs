99d0147ec714e0e53c4abd9ba3c6bd655614d431
Colin <karatekakid@hotmail.com>
2013-05-21 19:34:57 +0100

    First Checkin of Code
    Comparer and FileReader

---
+++ WordLadderSolver/WordLadderSolver/FileLoader.cs
@@ -0,0 +1,32 @@
+﻿namespace WordLadderSolver
+{
+    using System.IO;
+
+    /// <summary>
+    /// The file loader.
+    /// </summary>
+    public class DictionaryWordLoader
+    {
+        private readonly string _filePath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DictionaryWordLoader"/> class.
+        /// </summary>
+        /// <param name="filePath">
+        /// The file path.
+        /// </param>
+        public DictionaryWordLoader(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        /// <summary>
+        /// Load the file into an array
+        /// </summary>
+        /// <returns>string array of dictionary letters</returns>
+       public string[] ReadFile()
+       {
+           return File.ReadAllLines(this._filePath);
+       }
+    }
+}
---
+++ WordLadderSolver/WordLadderSolver/Program.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="SSE">
+//   Fo Shizzle Productions 
+// </copyright>
+// <summary>
+//   Puzzle Solver for ETT
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WordLadderSolver
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using System.Linq;
+
+    /// <summary>
+    /// The Puzzle Solver - Attempting to Solving Word Ladder Problems Since 2013
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = "\\Data\\dictionary.txt";
+            string path = string.Concat(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), file);
+
+            var rootWord = "bill";
+            List<string> wordLadder = new List<string>();
+            Dictionary<string, int> letterCount = new Dictionary<string, int>();
+
+            // Start The Proceedrue
+            DictionaryWordLoader loader = new DictionaryWordLoader(path);
+            string[] wordlist = loader.ReadFile();
+
+            var result = ContainsWord(rootWord, ref wordlist);
+        }
+
+        private static bool ContainsWord(string word, ref string[] wordlist)
+        {
+           return wordlist.AsParallel().Contains(word);
+        }
+    }
+}
