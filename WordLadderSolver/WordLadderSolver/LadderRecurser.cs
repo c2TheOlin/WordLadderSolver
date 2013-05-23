21ad3d46c3c866eba321db134c13b2d26d9bd5ee
Colin <colin@github>
2013-05-23 01:28:42 +0100

    Search Changes & Output of ladders
    moved a way from parallel for dictionary listings- few races condition
    due to not threads incorrect use of variable in deeper recursion layers.
    Not entirely sure it would have significant performance gains either
    give size of set its being used on. Probably quite a few ways to improve
    and get this down to a more efficient run time.

--- WordLadderSolver/WordLadderSolver/LadderRecurser.cs
+++ WordLadderSolver/WordLadderSolver/LadderRecurser.cs
@@ -1,35 +1,43 @@
 ﻿namespace WordLadderSolver
 {
+    using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Text;
     using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
-
+    
     public class LadderRecurser
     {
-        private List<string> _shortestLadder;
-        private List<string> _longestLadder;
         private readonly string _rootWord;
-        string[] _wordDictionary;
+        private readonly bool _notforTheLulz;
+        private string[] _wordDictionary;
+        private bool foundWord = false;
+        private byte[] wordbytes = { 99, 97, 116, 115 };
 
         /// <summary>
-        /// Breadth First Search Recursive Algorithm
+        /// Ladder Search
         /// </summary>
         /// <param name="rootWord">root word for ladder</param>
         /// <param name="wordDictionary">the word collection</param>
-        public LadderRecurser(string rootWord, string[] wordDictionary)
+        public LadderRecurser(string rootWord, string[] wordDictionary, bool notforTheLulz)
         {
             _rootWord = rootWord;
             _wordDictionary = wordDictionary;
-            _shortestLadder = new List<string>();
-            _longestLadder = new List<string>();
+            _notforTheLulz = notforTheLulz;
         }
 
         public void CreateLadders()
         {
-             FindNextRung(_rootWord, new List<string>(), ref _wordDictionary);
+            if (_notforTheLulz)
+            { 
+                FindFirstRung(_rootWord, new List<string>(), ref _wordDictionary);
+                return;
+            }
+
+            FindLadder(_rootWord, new List<string>(), ref _wordDictionary);
         }
 
-        private void FindNextRung(string word, List<string> ladder, ref string[] wordDictionary)
+        private void FindLadder(string word, List<string> ladder, ref string[] wordDictionary)
         {
             List<string> rungWords = new List<string>();
             int charIndex = 0;
@@ -38,19 +46,112 @@
                 var regexString = word.Replace('.', charIndex);
                 Regex regex = new Regex(regexString);
 
-                // Use Parallel Foreach to build next rungs
-                Parallel.ForEach(wordDictionary, current => {
+                // Use Foreach to build next rungs
+                Array.ForEach(wordDictionary, current =>
+                {
                     if (regex.IsMatch(current))
                     {
-                        if (current == _rootWord) return;
-                        if (rungWords.Contains(current)) return;
+                        if (current == word || rungWords.Contains(current) || ladder.Contains(current)) return;
                         
                         rungWords.Add(current);
                     }
                 });
 
                 charIndex++;
+            }  
+
+            ladder.Add(word);
+
+            // end of ladder dont print
+            if (rungWords.Count == 0)
+            {
+                return;
+                //    // Output the ladder
+                //    Console.WriteLine("\n");
+                //    Console.WriteLine("=====New Ladder===");
+                //    ladder.ForEach(x =>
+                //    {
+
+                //        Console.WriteLine(x);
+
+                //    });
+                //    Console.WriteLine("=====Ladder End===");
+                //    return;
             }
+
+            rungWords.ForEach(current =>
+            {
+                
+                if (current == Encoding.ASCII.GetString(wordbytes))
+                {
+                    if (foundWord == true)
+                    {
+                        return;
+                    }
+
+                    foundWord = true;
+
+                    // Output the ladder
+                    Console.WriteLine("\n");
+                    Console.WriteLine("=====New Ladder===");
+                    ladder.ForEach(x =>
+                    {
+                        Console.WriteLine(x);
+                    });
+
+                    Console.WriteLine(current);
+                    Process.Start("http://tinyurl.com/ng587pc"); // Should not be visited without running programme first
+                    Console.WriteLine("===== Ladder To " + current + " Ends - May it brighten your day :D ===");
+                }
+
+                if (foundWord)
+                {
+                    return;
+                }
+                
+                // Call Recursive for spanning tree 
+                FindLadder(current, ladder, ref _wordDictionary);
+            });
+        }
+
+        private void FindFirstRung(string word, List<string> ladder, ref string[] wordDictionary)
+        {
+            List<string> rungWords = new List<string>();
+            int charIndex = 0;
+            foreach (char c in word)
+            {
+                Regex regex = new Regex(word.Replace('.', charIndex));
+
+                // Use Foreach to build next rungs - could use linq here
+                // possibly slower? would need to prove
+                Array.ForEach(wordDictionary, current =>
+                {
+                    if (regex.IsMatch(current))
+                    {
+                        if (current == word) return;
+                        if (rungWords.Contains(current)) return;
+                        if (ladder.Contains(current)) return;
+
+                        rungWords.Add(current);
+                    }
+                });
+
+                charIndex++;
+            }
+
+            ladder.Add(word);
+
+            // Since we're only doing first rung - otherwise recursive
+            int ladderIndex = 1;
+            rungWords.ForEach(current =>
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("===== Ladder " + ladderIndex + " ===");
+                Console.WriteLine(ladder[0]);
+                Console.WriteLine(current);
+                Console.WriteLine("===================");
+                ladderIndex++;
+            });
         }
     }
 }
--- WordLadderSolver/WordLadderSolver/Program.cs
+++ WordLadderSolver/WordLadderSolver/Program.cs
@@ -16,11 +16,32 @@ namespace WordLadderSolver
 
     /// <summary>
     /// The Puzzle Solver - Attempting to Solving Word Ladder Problems Since 2013
+    /// Im certain there are better algorthms to do this. 
     /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
+            Console.BufferHeight = Int16.MaxValue - 1;
+            Console.Write("Enter [1] Run short First Rung Example or... [2] For 'other' search\n");
+            bool waitForInput = true;
+            bool processChoice = true;
+            string userInput = string.Empty;
+            while(waitForInput)
+            {
+                userInput = Console.ReadLine();
+                if(userInput == "1" || userInput == "2")
+                {
+                    waitForInput = false;
+                    processChoice = (userInput == "2") ? false : true;
+                }
+                else
+                {
+                    Console.WriteLine("Try Again - you have 10 seconds to comply");
+                }
+            }
+
+            Console.WriteLine("==========Starting========");
             var file = "\\Data\\dictionary.txt";
             var rootWord = "bill";
 
@@ -28,9 +49,16 @@ namespace WordLadderSolver
             DictionaryWordLoader loader = new DictionaryWordLoader(string.Concat(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), file));
             string[] wordlist = loader.ReadFile();
 
-            // Kick of Recursive Algorithm
-            LadderRecurser ladders = new LadderRecurser(rootWord, wordlist);
+            // Kick of Search
+            LadderRecurser ladders = new LadderRecurser(rootWord, wordlist, processChoice);
             ladders.CreateLadders();
+
+            Console.WriteLine("\nFree 12 variety box of Crispy Creme's if I Get the Role....");
+            System.Threading.Thread.Sleep(4000);
+            Console.WriteLine("At least dont give it to Dan Bower..... what a bastard");
+            System.Threading.Thread.Sleep(2000);
+            Console.Write("Any character to finish");
+            var name = Console.ReadLine();
         }      
     }
 
